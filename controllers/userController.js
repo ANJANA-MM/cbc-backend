b33642fda1c3204f3585a32d674707cdd4cb95c8
Anjana Muthunayake <anjanamanohari96@gmail.com>
2024-12-17 18:49:44 +0530

    product module created✔ admin and customer verifiction functions were created✅✅

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,20 +7,62 @@ import jwt from "jsonwebtoken";
 export function createUser (req,res) {
    const newUserData = req.body
 
-   newUserData.password=bcrypt.hashSync(newUserData.password,10)
+   
 
+   if (newUserData.type=="admin"){
+    if (req.user==null){
+        res.json({
+            message:"Please login as administrator to create admin accounts"
+        })
+        return
+    }
+    if (req.user.type != "admin"){
+        res.json({
+            message:"Please login as administrator to create admin accounts"
+        })
+        return
+    
+       }
+    
+
+
+   }
+   
+   newUserData.password=bcrypt.hashSync(newUserData.password,10)
    const user = new User (newUserData)
    user.save().then(()=>{
     res.json({
         message:"User created"
     })
    }
-).catch(()=>{
-    res.json({
+   ).catch(()=>{
+     res.json({
         message:"User not created"
     })
 }
 )
+}
+
+
+export function isAdmin (req){
+    if (req.user==null){
+        return false
+    }
+
+    if (req.user.type!="admin"){
+        return false
+    }
+    return true
+}
+
+export function isCustomer(req){
+    if (req.user==null){
+        return false
+    }
+    if (req.user.type!="customer"){
+        return false
+    }
+    return true
 }
 
 export function loginUser (req,res){
